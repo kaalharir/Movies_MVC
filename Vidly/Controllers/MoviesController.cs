4705e284f4b9b201160a4e2702679d4bbfc0b702
kaal <kaalharir@iut-dhaka.edu>
2024-11-30 20:47:53 +0300

    custom routes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,9 +11,32 @@ namespace Vidly.Controllers
     {
         // GET: Movies/Random
         public ActionResult Random()
+        //public ViewResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //return new ViewResult();
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("");
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    //}
     }
 }
